55a54c2313c129908d859429c2a4452c27803440
bobchopperz <bobchopperz@users.noreply.github.com>
2025-10-16 18:04:23 +0700

    ambil isi endpoint sebelum pake axios

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    // Set the project root to the current directory to resolve ambiguity.
+    root: __dirname,
+  },
 };
 
 export default nextConfig;
