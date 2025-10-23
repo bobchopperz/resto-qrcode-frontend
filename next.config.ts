8499e2939ef857dbff20f5a6fc9d10061fe1043a
bobchopperz <bobchopperz@users.noreply.github.com>
2025-10-23 10:39:43 +0700

    checkpoint before re-code for mobile UI

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,18 @@ const nextConfig: NextConfig = {
     // Set the project root to the current directory to resolve ambiguity.
     root: __dirname,
   },
+
+  // ini dibutuhkan untuk cross origin
+
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: 'http://localhost:8000/:path*', // Ganti dengan URL backend kakak
+  //     },
+  //   ];
+  // },
+
 };
 
 export default nextConfig;
